bd1634a5b38738e89f77b1e7185cb304b271e96e
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-14 03:04:21 +0000

    Implementing a waterfall flow of callbacks in iss.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 // Call back function
 // 2 params - error and ip
@@ -13,11 +13,39 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:', ip);
 // });
 
-fetchCoordsByIP('162.245.144.188', (error, coords) => {
+// fetchCoordsByIP('162.245.144.188', (error, coords) => {
+//   if (error) {
+//     console.log(`It didn't work!`, error);
+//     return;
+//   } else {
+//     console.log(`It worked!`, coords);
+//   }
+// });
+
+// fetchISSFlyOverTimes({ latitude: '49.26200', longitude: '-123.09230' }, (error, flyOver) => {
+
+//   if (error) {
+//     console.log(`It didn't work!`, error);
+//     return;
+//   } else {
+//   //Looping through flyOver array to get the risetime value and converting it to date format and then using toLocaleString to convert it to vancouver time
+//     for (const rise of flyOver) {
+//       const time = new Date(rise.risetime * 1000);
+//       const timeZone = time.toLocaleString('en-GB', {timeZone: 'America/Vancouver', timeZoneName: 'short'})
+
+//       console.log(`It worked! Returned flyover times: ${timeZone} for ${rise.duration} seconds!`);
+//     }
+//   }
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log(`It didn't work!`, error);
-    return;
+    return console.log(`It didn't work!`, error);
   } else {
-    console.log(`It worked!`, coords);
+    for (const rise of passTimes) {
+      const time = new Date(rise.risetime * 1000);
+      const timeZone = time.toLocaleString('en-CA', {weekday: 'short', month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', hour12: false, minute: 'numeric', second: 'numeric', timeZone: 'America/Vancouver', timeZoneName: 'short'});
+      console.log(`Next pass at ${timeZone} for ${rise.duration} seconds!`);
+    }
   }
 });
--- iss.js
+++ iss.js
@@ -1,11 +1,34 @@
 const request = require('request');
 
+// this function is calling back all the fetch functions
+const nextISSTimesForMyLocation = function(callback) {
+  
+  fetchMyIP((err, ip) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    fetchCoordsByIP(ip, (err, coords) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      fetchISSFlyOverTimes(coords, (err, passTimes) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        callback(null, passTimes);
+      });
+    });
+  });
+};
+
 const fetchMyIP = function(callback) {
  
   request(
     'https://api.ipify.org?format=json', (err, data, body) => {
       if (err) {
-       
         callback(err, null);
         return;
       }
@@ -15,14 +38,12 @@ const fetchMyIP = function(callback) {
         return;
       }
       callback(null, JSON.parse(body).ip);
-      
     });
 };
 
 const fetchCoordsByIP = function(ip, callback) {
  
   request('https://ipvigilante.com/' + ip, (err, data, body) => {
-    console.log(ip);
 
     if (err) {
       callback(err, null);
@@ -33,12 +54,29 @@ const fetchCoordsByIP = function(ip, callback) {
       callback(Error(msg), null);
       return;
     }
-    let latLong = {
+    let coords = {
       latitude: JSON.parse(body).data.latitude,
       longitude: JSON.parse(body).data.longitude
     };
-    callback(null, latLong);
+    callback(null, coords);
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates.`;
+      callback(Error(msg), null);
+      return;
+    }
+    const flyOver = JSON.parse(body).response;
+    callback(null, flyOver);
+
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
